29f8ed88aa2cfe0386fcea5f21728ee2a4cbcc9b
akhilo95 <akhilo@ymail.com>
2016-01-15 23:58:55 +0530

    Create pongal.c

---
+++ pongal.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<unistd.h>
+
+
+int main(void)
+ {
+ 
+system(clear);   
+int i;
+
+char a[]={"HAPPY PONGAL"};
+    
+ for(i=0;i<12;i++)
+    
+  { 
+     ("%c",a[i]);
+     
+    sleep(1);
+
+   fflush(stdout);    
+    
+	
+
+  }
+
+
+}
